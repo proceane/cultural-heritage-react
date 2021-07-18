cd7f13732e0bff873074497cc9df24e405d8f1ed
proceane <proceane1@gmail.com>
2021-07-18 13:51:26 +0900

    add : firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase";
+import "firebase/firebase";
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBSBdvvzT0xLv4OrXuxcBYGnC7aL0xev3Q",
+    authDomain: "cultural-heritage-71c69.firebaseapp.com",
+    databaseURL: "https://cultural-heritage-71c69-default-rtdb.firebaseio.com",
+    projectId: "cultural-heritage-71c69",
+    storageBucket: "cultural-heritage-71c69.appspot.com",
+    messagingSenderId: "141681582514",
+    appId: "1:141681582514:web:1718f01c442210d49bc4ab",
+    measurementId: "G-99CTZRYZFG"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
