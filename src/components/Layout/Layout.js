336e3df5fdda2c18834871cc009d2565c9c4451c
proceane <proceane1@gmail.com>
2021-07-20 20:21:23 +0900

    add : package.json, feat : add route, render()

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import withRouter from 'react-router';
+import {Switch, Route, withRouter} from 'react-router';
 
 import Header from '../Header';
 import Footer from '../Footer';
@@ -9,7 +9,19 @@ import Search from '../../pages/search';
 import Review from '../../pages/review';
 
 class Layout extends React.Component {
-
+    render() {
+        return(
+            <div>
+                <Header/>
+                <Switch>
+                    <Route path="/" component={Main}/>
+                    <Route path="/search" component={Search}/>
+                    <Route path="/review" component={Review}/>
+                </Switch>
+                <Footer/>
+            </div>
+        )
+    }
 }
 
 export default withRouter(Layout);
