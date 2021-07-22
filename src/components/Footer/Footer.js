715821d98d5a22d5528c745467aade94f112f437
proceane <proceane1@gmail.com>
2021-07-22 13:41:02 +0900

    add : css, image, feat : setting html in header, footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import '../../assets/css/layout.css';
 
 class Footer extends React.Component {
     render() {
         return(
-            <div>Footer</div>
+            <div className={"wrapper row5"}>
+                <div id="copyright" className={"hoc clear"}> 
+                    <p className={"fl_left"}>Copyright &copy; 2021 - All Rights Reserved - <a href="/">아름누리</a></p>
+                    <p className={"fl_right"}>Template by <a target="_blank" rel="noreferrer" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+                </div>
+            </div>  
         )
     }
 }
