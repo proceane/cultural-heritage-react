1be6ce2b9f4092847fad0e22e13e9af78a3ea36c
proceane <proceane1@gmail.com>
2021-07-23 13:59:42 +0900

    feat : implement of register page

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -5,7 +5,34 @@ import { withRouter } from 'react-router-dom';
 class Register extends React.Component {
     render() {
         return(
-            <div>Register</div>
+            <div class="wrapper row3">
+                <main class="hoc container clear"> 
+                    <div class="content"> 
+                        <div id="comments"> 
+                            <form action="#" method="post" onsubmit="return validation()">
+                                <div class="one_third first">
+                                    <label for="email">이메일 <span>*</span></label>
+                                    <input type="email" name="email" id="email" value="" size="22" required></input>
+                                </div>
+                                <div class="one_third">
+                                    <label for="pw_1">비밀번호(숫자, 영문 포함 8 ~ 16자) <span>*</span></label>
+                                    <input type="password" name="pw_1" id="pw_1" value="" size="22" required></input>
+                                </div>
+                                <div class="one_third">
+                                    <label for="pw_2">비밀번호 한 번 더 <span>*</span></label>
+                                    <input type="password" name="pw_2" id="pw_2" value="" size="22" required></input>
+                                </div>
+                                <div>
+                                    <input type="submit" name="submit" value="회원가입"></input>
+                                    &nbsp;
+                                    <input type="reset" name="reset" value="다시 입력"></input>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </main>
+            </div>
         )
     }
 }
