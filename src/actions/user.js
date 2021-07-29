a0bf943febb899c23f56deb75348522f21bb3108
proceane <proceane1@gmail.com>
2021-07-29 21:09:34 +0900

    feat : add login code

--- src/actions/user.js
+++ src/actions/user.js
@@ -3,6 +3,19 @@ export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
 export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
 
+export function receiveLogin() {
+    return {
+        type: LOGIN_SUCCESS
+    };
+}
+
+function loginError(payload) {
+    return {
+        type: LOGIN_FAILURE,
+        payload
+    }
+}
+
 function requestLogout() {
     return {
         type: LOGOUT_REQUEST
@@ -15,10 +28,25 @@ export function receiveLogout() {
     }
 }
 
+
+// 로그아웃
 export function logoutUser() {
     return (dispatch) => {
         dispatch(requestLogout());
         localStorage.removeItem('authenticated');
         dispatch(receiveLogout());
     };
+}
+
+// 로그인
+export function loginUser(creds){
+    return (dispatch) => {
+        dispatch(receiveLogin());
+
+        if (creds.email.length > 0 && creds.password.length > 0) {
+            localStorage.setItem('authenticated', true);
+        } else {
+            dispatch(loginError('Something was wrong. Try again'));
+        }
+    }
 }
