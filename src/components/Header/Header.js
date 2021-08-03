ecd84d705697888733fd13d09861e5f8713de51f
proceane <proceane1@gmail.com>
2021-08-04 00:01:04 +0900

    add : reactstrap, feat : use reactstrap in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import '../../assets/css/layout.css';
 import mainImage from '../../assets/images/main.jpg';
 import Login from "../../pages/login";
 import { logoutUser } from '../../actions/user';
+import { Navbar, Nav, NavItem, NavLink } from "reactstrap";
 
 class Header extends React.Component {
     loginState = Login.isAuthenticated(JSON.parse(localStorage.getItem("authenticated")));
@@ -14,16 +15,27 @@ class Header extends React.Component {
                         <div id="logo" className={"fl_left"}> 
                         <h1><a href="/">아름누리</a></h1>
                         </div>
-                        <nav id="mainav" className={"fl_right"}> 
+                        
+                        <Navbar id="mainav" className="fl_right">
+                            <Nav className={"clear"}>
+                                <NavItem className={"active"}>
+                                    <NavLink href="/">홈</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/search">검색</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/review">리뷰</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/mypage">마이페이지</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/login">로그인</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Navbar>
                         {/* 메뉴에 따라 active */}
-                        <ul className={"clear"}>
-                            <li className={"active"}><a href="/">홈</a></li>
-                            <li><a href="/search">검색</a></li>
-                            <li><a href="/review">리뷰</a></li>
-                            <li><a href="/mypage">마이페이지</a></li>
-                            <li><a href="/login">로그인</a></li>
-                        </ul>
-                        </nav>
                     </header>
                 </div>
                 {/* 메뉴에 따라 동적 변경 */}
